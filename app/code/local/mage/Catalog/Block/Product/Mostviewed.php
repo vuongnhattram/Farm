dfd482b95430fd93093f73be902db838ea48233d
hiep.tran <hiep.tran@wahsis.com>
2016-07-22 23:18:28 +0700

    checck billing

--- app/code/local/mage/Catalog/Block/Product/Mostviewed.php
+++ app/code/local/mage/Catalog/Block/Product/Mostviewed.php
@@ -1,6 +1,10 @@
 <?php
+
+
+
 class Mage_Catalog_Block_Product_Mostviewed extends Mage_Catalog_Block_Product_Abstract{
     public function __construct(){
+
         parent::__construct();
         $storeId    = Mage::app()->getStore()->getId();
         $products = Mage::getResourceModel('reports/product_collection')
@@ -15,6 +19,22 @@ class Mage_Catalog_Block_Product_Mostviewed extends Mage_Catalog_Block_Product_A
 
         $products->setPageSize(5)->setCurPage(1);
         $this->setProductCollection($products);
+    } }
+class Mage_Catalog_Block_Product_Bestseller extends Mage_Catalog_Block_Product_Abstract{
+    public function __construct(){
+        parent::__construct();
+        $storeId = Mage::app()->getStore()->getId();
+        $products = Mage::getResourceModel('reports/product_collection')
+            ->addOrderedQty()
+            ->addAttributeToSelect('*')
+            ->addAttributeToSelect(array('name', 'price', 'small_image'))
+            ->setStoreId($storeId)
+            ->addStoreFilter($storeId)
+            ->setOrder('ordered_qty', 'desc'); // most best sellers on top
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($products);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($products);
+
+        $products->setPageSize(3)->setCurPage(1);
+        $this->setProductCollection($products);
     }
 }
-?>
